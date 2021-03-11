c199eb53bac5d2fe0830b7e2f5aae59d8a8bda40
Michael Gauthier <mike@silverorange.com>
2021-03-11 00:18:16 -0400

    Remove unused paths

--- src/paths.ts
+++ src/paths.ts
@@ -13,7 +13,6 @@ export default {
   work: workDir,
   vendors: vendorDirs,
   packages: packagesDir,
-  composerLock: 'composer.lock',
   less: [
     'www/styles/*.less',
     'www/styles/**/*.less',
@@ -28,11 +27,7 @@ export default {
     'www/*.php',
     'www/admin/*.php',
   ],
-  min: 'www/min',
   compiled: 'www/compiled',
-  minified: 'www/min',
   compiledFlag: 'www/.concentrate-compiled',
-  minifiedFlag: 'www/.concentrate-minified',
-  combinedFlag: 'www/.concentrate-combined',
   symlinkSuffix: '.original',
 };
